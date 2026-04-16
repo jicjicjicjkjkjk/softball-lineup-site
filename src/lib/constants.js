dcb2be9ab3892bda882f2f86bbecc875fc320d42
jicjicjicjkjkjk <jkayse@hotmail.com>
2026-04-16 10:39:01 -0500

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,5 @@
+export const TEAM_ID = 'f76ea5a1-7c44-4789-bfbd-9771edd54f10'
 
+export const ATTENDANCE_SEASON_OPTIONS = ['In Season', 'Out of Season']
+export const ATTENDANCE_TYPE_OPTIONS = ['Pitchers/Catchers', 'Team Practice']
+export const ATTENDANCE_SURFACE_OPTIONS = ['Indoor', 'Outdoor']
