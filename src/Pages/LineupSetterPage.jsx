a44a0f0263a3915b9e7a9fef58bfe99290eb58dc
jicjicjicjkjkjk <jkayse@hotmail.com>
2026-04-22 16:04:40 -0500

    Update LineupSetterPage.jsx

--- src/Pages/LineupSetterPage.jsx
+++ src/Pages/LineupSetterPage.jsx
@@ -442,6 +442,7 @@ export default function LineupSetterPage({
         togglePreviewInningLock={togglePreviewInningLock}
         gameTypeOptions={gameTypeOptions}
         seasonOptions={seasonOptions}
+        trackingPriorityByPositionRows={trackingPriorityByPositionRows}
       />
 
       <div className="card">
