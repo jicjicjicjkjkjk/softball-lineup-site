ec3175ab71a9357fbd849c5a9c0099f0eaf4744d
jicjicjicjkjkjk <jkayse@hotmail.com>
2026-05-07 16:41:54 -0500

    Create lineupConstants.js

---
+++ src/lib/lineupConstants.js
@@ -0,0 +1,10 @@
+// src/lib/lineupConstants.js
+
+export const FIELD_POSITIONS = ['P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
+export const GRID_OPTIONS = ['', 'P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'Out', 'Injury']
+export const PRIORITY_POSITIONS = ['P', 'C', '1B', '2B', '3B', 'SS', 'OF']
+export const ALLOWED_POSITIONS = ['P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
+export const GAME_TYPES = ['Friendly', 'Tournament Pool', 'Tournament Bracket', 'Doubleheader', 'Round Robin']
+export const SEASON_BUCKETS = ['In Season', 'Out of Season']
+export const PRACTICE_TYPES = ['Pitchers/Catchers', 'Team Practice', 'Indoor Work', 'Outdoor Practice']
+export const SETTING_TYPES = ['Indoor', 'Outdoor']
